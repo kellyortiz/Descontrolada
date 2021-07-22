70453fbeb28636ed3efdda03d99fb2b40a0c122a
Kelly Ortiz Figueiredo <31560921@mackenzista.com.br>
2021-07-22 15:55:22 -0300

    Criação do CRUD

---
+++ Models/Descontrolada.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel
+   .DataAnnotations;
+using System.ComponentModel
+  .DataAnnotations.Schema;
+
+namespace codeBlox.Models
+{
+    [Table("descontroladaBD")]
+    public class Descontrolada
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        [Display(Name = "Preço de venda")]
+        public float preco { get; set; }
+        [Display(Name = "Descrição")]
+        public String descricao { get; set; }
+        public int Quantidade { get; set; }
+
+        [Display(Name = "Tipo")]
+        public string tipoProduto { get; set; }
+
+        [Display(Name = "Data de Cadastro")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime DtCadastro { get; set; }
+    }
+}
